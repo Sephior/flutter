7d6a77153a2ed7680844ab901a04356d1127aba1
Sephior <k97209735@gmail.com>
2023-08-07 21:36:44 +0900

    수정 기능 업데이트, 진척도 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
+  // 기본적인 정보를 
   Widget build(BuildContext context) {
     return MaterialApp(
       //제목
       title: 'To Do List',
       theme: ThemeData(
-        //제목창 기본 색상
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //기본 색상 설정
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+        textTheme: const TextTheme(
+          //textstyle 설정 가능
+          bodyMedium: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+          labelMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          ),
         useMaterial3: true,
       ),
       //제목
@@ -51,6 +57,26 @@ class _MyHomePageState extends State<MyHomePage> {
   //옆에 만든 task.dart에서 import해서 리스트 생성
   List<Task> tasks = [];
 
+  // 변수 생성
+  bool isModifying = false;
+  int modifyingIndex = 0;
+  double percent = 0.0;
+  
+  // 퍼센트 재설정 함수
+  void updatePercent(){
+    if (tasks.isEmpty){
+      percent = 0.0;
+    } else {
+      var completeTaskCnt = 0;
+      for (var i = 0; i<tasks.length; i++){
+        if (tasks[i].isComplete){
+          completeTaskCnt+=1;
+        }
+      }
+    percent = completeTaskCnt/tasks.length;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,102 +87,125 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        //자식 생성
-        child: Column(
-          //메인 축 정렬
-          mainAxisAlignment: MainAxisAlignment.start,
-          //자식의 자식 생성
-          children: [
-            //텍스트
-            Text(getToday()),
-            //padding, 감싸기
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              //한 열에 여러 개의 자식을 생성
-              child: Row(
-                children:[
-                  //flexible 세팅
-                  Flexible(
-                    //텍스트 필드(텍스트 입력창 생성)
-                    child: TextField(
-                      controller: _textController,
+      body: SingleChildScrollView(
+        child: Center(
+          //자식 생성
+          child: Column(
+            //메인 축 정렬
+            mainAxisAlignment: MainAxisAlignment.start,
+            //자식의 자식 생성
+            children: [
+              //텍스트
+              Text(getToday()),
+              //padding, 감싸기
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                //한 열에 여러 개의 자식을 생성
+                child: Row(
+                  children:[
+                    //flexible 세팅
+                    Flexible(
+                      //텍스트 필드(텍스트 입력창 생성)
+                      child: TextField(
+                        controller: _textController,
+                        ),
                       ),
-                    ),
-                  //버튼 생성
-                  ElevatedButton(
-                    onPressed: () {
-                      //버튼의 효과 설정
-                      if (_textController.text != '') {
-                        setState(() {
-                          var task = Task(_textController.text);
-                          tasks.add(task);
-                          _textController.clear();
-                        });
-                      }
-                    },
-                    //버튼 내부 데이터 설정
-                    child: const Text("Add"
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                LinearPercentIndicator(
-                  //현재 기기의 설정 불러오기 context
-                  width: MediaQuery.of(context).size.width - 50,
-                  lineHeight: 14,
-                  percent: 0.5,
-                )
-              ]),
-            ),
-
-            for (var i = 0; i < tasks.length; i++)
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  //텍스트 버튼 생성
-                  Flexible(child: TextButton(
-                    style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.zero)
+                    //버튼 생성
+                    ElevatedButton(
+                      onPressed: () {
+                        //버튼의 효과 설정
+                        if (_textController.text != '') {
+                          // isModifying 이 true이면 ? 이후의 코드가, false면 : 이후의 코드가 실행됨
+                          isModifying
+                          ? setState(() {
+                            tasks[modifyingIndex].work = _textController.text;
+                            tasks[modifyingIndex].isComplete = false;
+                            _textController.clear();
+                            modifyingIndex = 0;
+                            isModifying = false;
+                          })
+                          : setState((){
+                            var task = Task(_textController.text);
+                            tasks.add(task);
+                            _textController.clear();
+                          });
+                        }
+                      },
+                      //버튼 내부 데이터 설정
+                      child: isModifying
+                      ? const Text("revise")
+                      : const Text("Add"),
                       )
-                    ),
-                    onPressed: () {},
-                    child: Row(
-                      children: [
-                        const Icon(Icons.check_box_outline_blank_rounded),
-                        Text(tasks[i].work)
-                      ],
-                    ),
+                    ],
                   ),
                 ),
-                TextButton(onPressed: (){
-                  setState(() {
-                    if (_textController.text != '') {
-                      var task = Task(_textController.text);
-                      tasks[i] = task;
-                      _textController.clear();
-                    }
-                  });
-                }, child: const Text("revise"),),
-
-                TextButton(onPressed: (){
-                  setState(() {
-                    tasks.remove(tasks[i]);
-                  });
-                }, child: const Text("remove"),),
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  LinearPercentIndicator(
+                    //현재 기기의 설정 불러오기 context
+                    width: MediaQuery.of(context).size.width - 50,
+                    lineHeight: 14,
+                    percent: percent,
+                  )
+                ]),
               ),
-            )
-          ],
+              
+              // 반복문
+              for (var i = 0; i < tasks.length; i++)
+      
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    //텍스트 버튼 생성
+                    Flexible(child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero)
+                        )
+                      ),
+                      onPressed: () {
+                        setState(() {
+                        tasks[i].isComplete = !tasks[i].isComplete;
+                        updatePercent();
+                        });
+                      },
+                      child: Row(
+                        children: [
+                          tasks[i].isComplete
+                          // ?면 True일 때 체크된 모양
+                          ?  const Icon(Icons.check_box_rounded)
+                          // : 이면 False일 때 체크된 모양
+                          :  const Icon(Icons.check_box_outline_blank_rounded),
+                          Text(tasks[i].work, style: Theme.of(context).textTheme.labelMedium)
+                        ],
+                      ),
+                    ),
+                  ),
+                  TextButton(onPressed: isModifying
+                    ? null
+                    : (){
+                      setState(() {
+                        isModifying = true;
+                        _textController.text = tasks[i].work;
+                        modifyingIndex = i;
+                        updatePercent();
+                      });
+                  }, child: const Text("revise"),),
+                  TextButton(onPressed: (){
+                    setState(() {
+                      tasks.remove(tasks[i]);
+                      updatePercent();
+                    });
+                  }, child: const Text("remove"),),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
